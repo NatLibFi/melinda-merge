3c5b6156ebfc64e13c7c3fe1769444dcdcd88ef7
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-09-01 12:16:26 +0300

    Adds react router, signin and app are in different routes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,12 +2,16 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BaseComponent } from './components/base-component';
+import { SigninFormPanel } from './components/signin-form-panel';
 
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
 import {Provider} from 'react-redux';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/app';
+
 
 const loggerMiddleware = createLogger();
 
@@ -19,10 +23,19 @@ const store = createStore(
   )
 );
 
+const routes = (
+  <Route component={App}>
+    <Route path='/signin' component={SigninFormPanel} />
+    <Route path='/' component={BaseComponent} />
+  </Route>
+);
+
 const rootElement = document.getElementById('app');
 
 ReactDOM.render(
   <Provider store={store}>
-    <BaseComponent />
-  </Provider>, rootElement);
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>, 
+  rootElement
+);
 
