f5265e55d9e45c5f22c790ef07e7215cd42d30a7
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-09-02 11:36:08 +0300

    Adds option to hide values from logfiles

--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,7 @@
 import { logger } from './logger';
 import _ from 'lodash';
 
-export function readEnvironmentVariable(name, defaultValue) {
+export function readEnvironmentVariable(name, defaultValue, opts) {
 
   if (process.env[name] === undefined) {
     if (defaultValue === undefined) {
@@ -10,7 +10,8 @@ export function readEnvironmentVariable(name, defaultValue) {
       logger.log('error', message);
       throw new Error(message);
     }
-    logger.log('info', `No environment variable set for ${name}, using default value: ${defaultValue}`);
+    const loggedDefaultValue = _.get(opts, 'hideDefaultValue') ? '[hidden]' : defaultValue;
+    logger.log('info', `No environment variable set for ${name}, using default value: ${loggedDefaultValue}`);
   }
 
   return _.get(process.env, name, defaultValue);
