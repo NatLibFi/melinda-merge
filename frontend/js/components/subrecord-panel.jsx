35af9117dab8838819e3112521f7aad3c8c8e6af
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-10-07 11:01:05 +0300

    Adds bright colors to subrecords

--- frontend/js/components/subrecord-panel.jsx
+++ frontend/js/components/subrecord-panel.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import classNames from 'classnames';
 import '../../styles/components/subrecord-panel';
+import { MarcRecordPanel } from './marc-record-panel';
+import _ from 'lodash';
 
 export class SubRecordPanel extends React.Component {
 
   static propTypes = {
     record: React.PropTypes.object.isRequired,
+    type: React.PropTypes.string.isRequired,
     isDragging: React.PropTypes.bool
   }
 
   render() {
-
     const { record, isDragging } = this.props;
-
-    const recordString = record ? record.toString() : '';
-
-    const selectedFields = recordString
-      .split('\n')
-      .filter(f => f.substr(0,3) === '245' || f.substr(0,3) === '336')
-      .join('\n');
+    const selectedFields = record.fields
+      .filter(f => _.includes(['245', '336'], f.tag));
 
     const classes = classNames({
       'is-dragging': isDragging,
       'card-panel': true,
       'darken-1': true,
-      'marc-record': true
+      'marc-record': true,
+      'marc-record-subrecord': true,
+      'marc-record-source': this.props.type == 'SOURCE',
+      'marc-record-target': this.props.type == 'TARGET',
+      'marc-record-merged': this.props.type == 'MERGED'
     });
+
+    const trimmedRecord = {
+      fields: selectedFields
+    };
 
     return (
       <div className={classes}>
-        <div>{selectedFields}</div>
+        <MarcRecordPanel record={trimmedRecord} />
       </div>
     );
   }
