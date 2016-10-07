d0f86739b125eaba0bb9f694a16b56a3db9826c6
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-10-07 15:13:12 +0300

    Adds archive service which creates archives of succesful merges

---
+++ server/archive-service.js
@@ -0,0 +1,79 @@
+import fs from 'fs';
+import path from 'path';
+import archiver from 'archiver';
+import mkdirp from 'mkdirp';
+import moment from 'moment';
+import { readEnvironmentVariable } from './utils';
+import _ from 'lodash';
+
+const defaultArchivePath = path.resolve(__dirname, '..', 'merge-action-archive');
+const archivePath = readEnvironmentVariable('ARCHIVE_PATH', defaultArchivePath);
+mkdirp.sync(archivePath);
+
+export function createArchive(user, removedRecord, preferredRecord, mergedRecord, mergedRecordId) {
+
+  return new Promise(function(resolve, reject) {
+
+    const timeStamp = new Date().getTime();
+    const removedRecordId = getRecordId(removedRecord.record);
+    const preferredRecordId = getRecordId(preferredRecord.record);
+    const username = user;
+    const date = moment().format();
+
+    const metadata = {
+      date,
+      username,
+      removedRecordId,
+      preferredRecordId,
+      mergedRecordId
+    };
+
+    const filename = `merge-${removedRecordId}-${preferredRecordId}-${mergedRecordId}-${username}-${timeStamp}`;
+
+    const output = fs.createWriteStream(path.resolve(archivePath, `${filename}.zip`));
+    const archive = archiver('zip');
+
+    output.on('close', function() {
+      resolve({
+        filename: `${filename}.zip`,
+        size: archive.pointer()
+      });
+    });
+
+    archive.on('error', function(err) {
+      reject(err);
+    });
+
+    archive.pipe(output);
+
+    archive
+      .append(removedRecord.record.toString(), {name: 'removed.txt'})
+      .append(preferredRecord.record.toString(), {name: 'preferred.txt'})
+      .append(mergedRecord.record.toString(), {name: 'merged.txt'})
+      .append(JSON.stringify(metadata), {name: 'meta.json'});
+
+
+    if (removedRecord.subrecords && removedRecord.subrecords.length) {
+      archive.append(removedRecord.subrecords.map(asString).join('\n'), {name: 'removed-subrecords.txt'});
+    }
+    if (preferredRecord.subrecords && preferredRecord.subrecords.length) {
+      archive.append(preferredRecord.subrecords.map(asString).join('\n'), {name: 'preferred-subrecords.txt'});
+    }
+    if (mergedRecord.subrecords && mergedRecord.subrecords.length) {
+      archive.append(mergedRecord.subrecords.map(asString).join('\n'), {name: 'merged-subrecords.txt'});
+    }
+        
+    archive.finalize();
+
+  });
+
+}
+
+function asString(record) {
+  return record.toString();
+}
+
+function getRecordId(record) {
+  return _.head(record.fields.filter(field => field.tag === '001').map(field => field.value));
+}
+
