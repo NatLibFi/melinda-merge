a5ab7bdd572dc055fc13dbc4a007dfefdfeecf95
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-12-02 12:47:30 +0200

    Show 773 info

--- frontend/js/components/subrecord/subrecord-panel.jsx
+++ frontend/js/components/subrecord/subrecord-panel.jsx
@@ -22,9 +22,10 @@ export class SubRecordPanel extends React.Component {
   render() {
     const { record, isDragging, isExpanded } = this.props;
 
-
     const selectedFields = record.fields
-      .filter(f => _.includes(['245', '336'], f.tag));
+      .filter(f => _.includes(['245', '336', '773'], f.tag))
+      .map(toOnlySubfields('773', ['g','q']))
+      .filter(f => f.subfields.length !== 0);
 
     const classes = classNames({
       'is-dragging': isDragging,
@@ -68,4 +69,15 @@ export class SubRecordPanel extends React.Component {
     }
 
   }
+}
+
+function toOnlySubfields(tag, subfieldCodes) {
+  return function(field) {
+    if (field.tag === tag) { 
+      const subfields = field.subfields.filter(s => _.includes(subfieldCodes, s.code));
+      return _.assign({}, field, {subfields});
+    }
+    return field;
+    
+  };
 }
