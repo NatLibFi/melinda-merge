ddd3d243f6c8903f18caa7e4f478e8a5a5909cdf
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-12-08 11:15:00 +0200

    Removes obsolete code

--- frontend/js/selectors/subrecord-selectors.js
+++ frontend/js/selectors/subrecord-selectors.js
@@ -2,26 +2,6 @@ import _ from 'lodash';
 import { createSelector } from 'reselect';
 import { List } from 'immutable';
 
-const sourceRecordList = state => state.getIn(['subrecords',  'sourceRecord'], List()).toJS();
-const targetRecordList = state => state.getIn(['subrecords',  'targetRecord'], List()).toJS();
-const mergedRecordList = state => state.getIn(['subrecords',  'mergedRecord'], List()).toJS();
-const actionsList = state => state.getIn(['subrecords',  'actions'], List()).toJS();
-const expandedList = state => state.getIn(['subrecords', 'expanded'], List()).toJS();
-
-export const subrecordRows2 = createSelector(
-  [sourceRecordList, targetRecordList, mergedRecordList, actionsList, expandedList], 
-  (sourceRecordList, targetRecordList, mergedRecordList, actionsList, expandedList) => {
-
-    return _.zip(sourceRecordList, targetRecordList, mergedRecordList, actionsList, expandedList)
-      .map(([sourceRecord, targetRecord, mergedRecord, selectedAction, isExpanded]) => {
-        return {
-          sourceRecord, targetRecord, mergedRecord, selectedAction, isExpanded
-        };
-      });
-
-  }
-);
-
 export const subrecordOrder = state => state.getIn(['subrecords', 'index']).toJS();
 export const subrecords = state => state.get('subrecords').delete('index').toJS();
 
