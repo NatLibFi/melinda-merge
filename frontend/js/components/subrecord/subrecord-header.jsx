f66d142743a07b5ac51b3e6e2fd25e98f7ca3483
Pasi Tuominen <pasi.tuominen@gmail.com>
2016-12-09 14:22:32 +0200

    Adds checkbox for compacting

--- frontend/js/components/subrecord/subrecord-header.jsx
+++ frontend/js/components/subrecord/subrecord-header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import * as uiActionCreators from '../../ui-actions';
+import { setCompactSubrecordView } from '../../ui-actions';
 import {connect} from 'react-redux';
 
 export class SubrecordHeader extends React.Component {
 
+  static propTypes = {
+    setCompactSubrecordView: React.PropTypes.func.isRequired,
+    compactSubrecordView: React.PropTypes.bool.isRequired
+  }
+  
+  toggleCompactView(event) {
+    const isEnabled = event.target.checked;
+
+    this.props.setCompactSubrecordView(isEnabled);    
+  }
+
   render() {
+
     return (
       
       <div className="row subrecord-header">
@@ -13,41 +25,42 @@ export class SubrecordHeader extends React.Component {
           <div className="row">
 
             <div className="col s4"><h5>Osakohteet</h5></div>
-            <div className="col s4 right">
-               <select>
-                <option value="" disabled>Järjestä</option>
-                <option value="1">id</option>
-                <option value="2">nimi</option>
-                <option value="3">muu</option>
-              </select>
-
-            </div>
+           
           </div>
         </div>
+
+        <div className="col s4" />
 
         <div className="col s4">
-          <div className="row">
-            <div className="col s4 right">
-          
-              <select>
-                <option value="" disabled>Järjestä</option>
-                <option value="1">id</option>
-                <option value="2">nimi</option>
-                <option value="3">muu</option>
-              </select>
-
-            </div>
-          </div>
+          <p>
+            <input 
+              type="checkbox" 
+              id="compact-subrecords-checkbox" 
+              value="compact-subrecords-view"
+              className="filled-in"
+              checked={this.props.compactSubrecordView} 
+              onChange={(e) => this.toggleCompactView(e)}
+              ref={(c) => this._compactViewCheckBox = c}
+            />
+            
+            <label htmlFor="compact-subrecords-checkbox">Pienennä käsitellyt osakohteet</label>
+          </p>
         </div>
-        <div className="col s4" />
       </div>
     );
   }
 }
 
+
+function mapStateToProps(state) {
+  return {
+    compactSubrecordView: state.getIn(['ui', 'compactSubrecordView'])
+  };
+}
+
 export const SubrecordHeaderContainer = connect(
-  null,
-  uiActionCreators
+  mapStateToProps,
+  { setCompactSubrecordView }
 )(SubrecordHeader);
 
 
