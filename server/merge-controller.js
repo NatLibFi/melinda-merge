0d2d8e91cf0317d06b1e17357b949e61fcfd4785
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2017-10-24 09:58:36 +0300

    Update melinda-api-client to @natlibfi scope

--- server/merge-controller.js
+++ server/merge-controller.js
@@ -39,7 +39,7 @@ import { readSessionMiddleware } from 'server/session-controller';
 import _ from 'lodash';
 import { createArchive } from './archive-service';
 
-const MelindaClient = require('melinda-api-client');
+const MelindaClient = require('@natlibfi/melinda-api-client');
 const alephUrl = readEnvironmentVariable('ALEPH_URL');
 const apiVersion = readEnvironmentVariable('MELINDA_API_VERSION', null);
 const apiPath = apiVersion !== null ? `/${apiVersion}` : '';
